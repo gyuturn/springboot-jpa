aa2c62690f41f577ab638a37ef21cffd74f6563f
turn <kksy2423@naver.com>
2022-02-10 01:15:52 +0900

    상품 주문

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -63,17 +63,9 @@ public class ItemController {
     }
 
     @PostMapping("items/{itemId}/edit")
-    public String updateItemForm(@ModelAttribute("form") BookForm form) {
+    public String updateItem(@PathVariable Long itemId,@ModelAttribute("form") BookForm form) {
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
 
-        Book book = new Book();
-        book.setAuthor(form.getAuthor());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setPrice(form.getPrice());
-        book.setName(form.getName());
-        book.setIsbn(form.getIsbn());
-        book.setId(form.getId());
-
-        itemService.saveItem(book);
         return "redirect:/items";
     }
 
